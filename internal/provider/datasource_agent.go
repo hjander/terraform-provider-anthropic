89c41df03d05f07293a7ffb7f23de82c868d225e
Hendrik Jander <hendrik.jander@skillscale.cloud>
2026-04-13 20:01:19 +0200

    refactor: decouple agent data source from resource model

--- internal/provider/datasource_agent.go
+++ internal/provider/datasource_agent.go
@@ -13,6 +13,21 @@ type agentDataSource struct {
 	client *Client
 }
 
+type agentDataSourceModel struct {
+	ID          types.String `tfsdk:"id"`
+	Name        types.String `tfsdk:"name"`
+	Description types.String `tfsdk:"description"`
+	ModelID     types.String `tfsdk:"model_id"`
+	ModelSpeed  types.String `tfsdk:"model_speed"`
+	System      types.String `tfsdk:"system"`
+	Metadata    types.Map    `tfsdk:"metadata"`
+	MCPServers  types.List   `tfsdk:"mcp_servers"`
+	Skills      types.List   `tfsdk:"skills"`
+	Tools       types.List   `tfsdk:"tools"`
+	Version     types.Int64  `tfsdk:"version"`
+	Archived    types.Bool   `tfsdk:"archived"`
+}
+
 var _ datasource.DataSource = (*agentDataSource)(nil)
 
 func NewAgentDataSource() datasource.DataSource { return &agentDataSource{} }
@@ -101,7 +116,7 @@ func (d *agentDataSource) Schema(_ context.Context, _ datasource.SchemaRequest, 
 }
 
 func (d *agentDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
-	var config agentResourceModel
+	var config agentDataSourceModel
 	resp.Diagnostics.Append(req.Config.Get(ctx, &config)...)
 	if resp.Diagnostics.HasError() {
 		return
@@ -113,7 +128,24 @@ func (d *agentDataSource) Read(ctx context.Context, req datasource.ReadRequest, 
 		return
 	}
 
-	state, diags := flattenAgentState(ctx, api)
+	flat, diags := flattenAgentState(ctx, api)
 	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	state := agentDataSourceModel{
+		ID:          flat.ID,
+		Name:        flat.Name,
+		Description: flat.Description,
+		ModelID:     flat.ModelID,
+		ModelSpeed:  flat.ModelSpeed,
+		System:      flat.System,
+		Metadata:    flat.Metadata,
+		MCPServers:  flat.MCPServers,
+		Skills:      flat.Skills,
+		Tools:       flat.Tools,
+		Version:     flat.Version,
+		Archived:    flat.Archived,
+	}
 	resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
 }
